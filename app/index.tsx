fd7034c0055cbe7884e02bc9da3f7ff531ed4151
Ian <ianmayw@gmail.com>
2026-01-06 20:36:22 -0800

    Add text and flatlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,44 @@
 import AppHeader from "@/components/AppHeader";
-import { StyleSheet, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 
+interface Watchlist {
+  id: string,
+  label: string,
+}
+
+const initialData: Watchlist[] = [
+  {
+    id: "1",
+    label: "Favorites"
+  },
+  {
+    id: "2",
+    label: "Trending"
+  },
+  {
+    id: "3",
+    label: "Watching"
+  }
+]
 
 export default function Index() {
+
   return (
     <View style={styles.container}>
       <AppHeader>
         Watchlists
       </AppHeader>
+      <Text style={styles.text}>{initialData[0].label}</Text>
+      <Text style={styles.text}>{initialData[1].label}</Text>
+      <Text style={styles.text}>{initialData[2].label}</Text>
+      <Pressable>
+        <Text style={styles.text}>
+          Click Me
+        </Text>
+      </Pressable>
+      <FlatList
+        data={initialData}
+        renderItem={({ item }) => <Text>{item.label}</Text>} />
     </View>
   );
 }
@@ -18,4 +49,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "gray",
   },
+  text: {
+    marginTop: 12,
+    fontSize: 24,
+  }
 })
